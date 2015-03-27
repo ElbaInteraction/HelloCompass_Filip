14143fe53bdc52c12cd8adeff320527e825865ae
fbontin <fbontin@gmail.com>
2015-03-27 13:02:23 +0100

    Design changes, circle around arrow with all directions.

--- app/src/main/java/com/example/filip/hellocompass/DisplayCompassActivity.java
+++ app/src/main/java/com/example/filip/hellocompass/DisplayCompassActivity.java
@@ -23,10 +23,10 @@ public class DisplayCompassActivity extends Activity implements SensorEventListe
 
         public CustomDrawableView(Context context) {
             super(context);
-            paint.setColor(0xff00ff00);
             paint.setStyle(Style.STROKE);
             paint.setStrokeWidth(2);
             paint.setAntiAlias(true);
+            paint.setTextSize(40f);
         }
 
         protected void onDraw(Canvas canvas) {
@@ -34,30 +34,53 @@ public class DisplayCompassActivity extends Activity implements SensorEventListe
             int height = getHeight();
             int centerx = width / 2;
             int centery = height / 2;
-            canvas.drawLine(centerx, 0, centerx, height, paint);
-            canvas.drawLine(0, centery, width, centery, paint);
+
+            paint.setStyle(Style.FILL);
+            paint.setColor(Color.LTGRAY);
+            canvas.drawRect(0, 0, width, height, paint);
+
+            paint.setColor(Color.GRAY);
+            canvas.drawCircle(centerx, centery, width/3, paint);
+            paint.setStyle(Style.STROKE);
+
+            paint.setColor(Color.BLUE);
+            canvas.drawLine(centerx, centery - width / 4, centerx, centery, paint); //arrow line
+
+
+            canvas.drawLine(centerx, centery - width / 3, centerx + 50, centery - width / 4, paint); //arrowhead right
+            canvas.drawLine(centerx, centery - width / 3, centerx - 50, centery - width / 4, paint); //arrowhead left
+            canvas.drawLine(centerx + 50, centery - width / 4, centerx - 50, centery - width / 4, paint); //arrowhead bottom
+
+            canvas.drawCircle(centerx, centery, width / 3, paint);
+
             // Rotate the canvas with the azimut
             if (azimut != null)
                 canvas.rotate(-azimut * 360 / (2 * 3.14159f), centerx, centery);
-            paint.setColor(0xff0000ff);
-            canvas.drawLine(centerx, centery - 263, centerx, centery - 10, paint); //vertical
-            //canvas.drawLine(-2000, centery, 2000, centery, paint); //horisontal
 
+            paint.setColor(Color.DKGRAY);
+            canvas.drawLine(centerx, centery + width / 3, centerx, centery - width / 3, paint);
+            canvas.drawLine(centerx + width / 3, centery, centerx - width / 3, centery, paint);
+
+            //draw text directions.
+            paint.setColor(Color.RED);
+
+            canvas.drawText("N", centerx - width / 60, centery - width / 3 - 2, paint);
+
+            canvas.rotate(90, centerx, centery);
+            canvas.drawText("E", centerx - width / 60, centery - width / 3 - 2, paint);
+
+            canvas.rotate(90, centerx, centery);
+            canvas.drawText("S", centerx - width / 60, centery - width / 3 - 2, paint);
+
+            canvas.rotate(90, centerx, centery);
+            canvas.drawText("W", centerx - width / 60, centery - width / 3 - 2, paint);
+
+            paint.setColor(Color.BLUE);
             paint.setStyle(Style.FILL);
             canvas.drawCircle(centerx, centery, 10, paint); //middle circle
             paint.setStyle(Style.STROKE);
 
-            canvas.drawLine(centerx, centery - 400, centerx + 50, centery - 300, paint); //arrowhead right
-            canvas.drawLine(centerx, centery - 400, centerx - 50, centery - 300, paint); //arrowhead left
-            canvas.drawLine(centerx + 50, centery - 300, centerx - 50, centery - 300, paint); //arrowhead bottom
 
-            paint.setTextSize(40f);
-            paint.setColor(Color.RED);
-            canvas.drawText("North", centerx - 50, centery - 265, paint);
-
-            //canvas.drawText("S", centerx - 10, centery + 15, paint);
-
-            paint.setColor(0xff00ff00);
         }
     }
 
